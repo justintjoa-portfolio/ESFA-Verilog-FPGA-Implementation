fc9ae76faf8a145b03237bfbecade06437f51d34
Justin Tjoa <justintoja@gmail.com>
2021-11-24 15:29:33 -0800

    update

--- ESFA.srcs/sim_1/new/esfa_test.v
+++ ESFA.srcs/sim_1/new/esfa_test.v
@@ -73,6 +73,7 @@ module esfa_test(
     
     initial
     begin
+        // update Basic test
         r_true <= (design.c0.new_rank == 0);
         
         //ESFAArrayOp().update(emptyArrayState, None, 0, 5)
@@ -482,9 +483,161 @@ module esfa_test(
         r_true <= (resultBool == 0); //there is no value with index 1 in sixth array
         
         //ESFAArrayOp().delete(state_and_handle._1, 1)
+        isHandle <= 1'b1;
+        queried_handle <= 1;
+        selector <= 4;
+        willWrite <= 1;
+        #28;
+        selector <= 6;
+        #28;
+        willWrite <= 0;
+        r_true <= (design.c0.new_arrDef == 1'b1);
+        r_true <= (design.c0.new_eltDef == 1'b1);
+        r_true <= (design.c0.new_mark == 1'b0);
+        r_true <= (design.c0.new_index == 0);
+        r_true <= (design.c0.new_value == 5);
+        r_true <= (design.c0.new_rank == 1);
+        r_true <= (design.c0.new_low == 0);
+        r_true <= (design.c0.new_high == 2);
+        r_true <= (design.c0.handle == 0);
+        r_true <= (design.c0.new_array_code == 0);
+        
+        r_true <= (design.c1.new_arrDef == 1'b0);
+        r_true <= (design.c1.new_eltDef == 1'b1);
+        r_true <= (design.c1.new_mark == 1'b0);
+        r_true <= (design.c1.new_index == 2);
+        r_true <= (design.c1.new_value == 10);
+        r_true <= (design.c1.new_rank == 0);
+        r_true <= (design.c1.new_low == 1);
+        r_true <= (design.c1.new_high == 2);
+        r_true <= (design.c1.handle == 1);
+        r_true <= (design.c1.new_array_code == 1);
+        
+        r_true <= (design.c3.new_arrDef == 1'b1);
+        r_true <= (design.c3.new_eltDef == 1'b1);
+        r_true <= (design.c3.new_mark == 1'b0);
+        r_true <= (design.c3.new_index == 10);
+        r_true <= (design.c3.new_value == 21);
+        r_true <= (design.c3.new_rank == 2);
+        r_true <= (design.c3.new_low == 4);
+        r_true <= (design.c3.new_high == 4);
+        r_true <= (design.c3.handle == 3);
+        r_true <= (design.c3.array_code == 4);
+        
+        r_true <= (design.c2.new_arrDef == 1'b1);
+        r_true <= (design.c2.new_eltDef == 1'b1);
+        r_true <= (design.c2.new_mark == 1'b0);
+        r_true <= (design.c2.new_index == 4);
+        r_true <= (design.c2.new_value == 10);
+        r_true <= (design.c2.new_rank == 1);
+        r_true <= (design.c2.new_low == 3);
+        r_true <= (design.c2.new_high == 4);
+        r_true <= (design.c2.handle == 2);
+        r_true <= (design.c2.new_array_code == 3);
+        
+        r_true <= (design.c4.new_arrDef == 1'b1);
+        r_true <= (design.c4.new_eltDef == 1'b1);
+        r_true <= (design.c4.new_mark == 1'b0);
+        r_true <= (design.c4.new_index == 9);
+        r_true <= (design.c4.new_value == 5);
+        r_true <= (design.c4.new_rank == 3);
+        r_true <= (design.c4.new_low == 2);
+        r_true <= (design.c4.new_high == 2);
+        r_true <= (design.c4.handle == 4);
+        r_true <= (design.c4.array_code == 2);
+        
+        r_true <= (design.c5.new_arrDef == 1'b1);
+        r_true <= (design.c5.new_eltDef == 1'b1);
+        r_true <= (design.c5.new_mark == 1'b0);
+        r_true <= (design.c5.new_index == 11);
+        r_true <= (design.c5.new_value == 14);
+        r_true <= (design.c5.new_rank == 3);
+        r_true <= (design.c5.new_low == 1);
+        r_true <= (design.c5.new_high == 1);
+        r_true <= (design.c5.handle == 5);
+        r_true <= (design.c5.array_code == 1);
+        
+        //ESFAArrayOp().delete(post_deletion_state, 4)
+        isHandle <= 1'b1;
+        queried_handle <= 4;
+        selector <= 4;
+        willWrite <= 1;
+        #28;
+        selector <= 6;
+        #28;
+        willWrite <= 0;
+        
+        //ESFAArrayOp().delete(post_deletion_state, 5)
+        isHandle <= 1'b1;
+        queried_handle <= 5;
+        selector <= 4;
+        willWrite <= 1;
+        #28;
+        selector <= 6;
+        #28;
+        willWrite <= 0;
+        
+        r_true <= (design.c0.new_arrDef == 1'b1);
+        r_true <= (design.c0.new_eltDef == 1'b1);
+        r_true <= (design.c0.new_mark == 1'b0);
+        r_true <= (design.c0.new_index == 0);
+        r_true <= (design.c0.new_value == 5);
+        r_true <= (design.c0.new_rank == 1);
+        r_true <= (design.c0.new_low == 0);
+        r_true <= (design.c0.new_high == 0);
+        r_true <= (design.c0.handle == 0);
+        r_true <= (design.c0.new_array_code == 0);
+        
+        r_true <= (design.c1.new_arrDef == 1'b0);
+        r_true <= (design.c1.new_eltDef == 1'b0);
+        r_true <= (design.c1.new_mark == 1'b0);
+        
+        r_true <= (design.c3.new_arrDef == 1'b1);
+        r_true <= (design.c3.new_eltDef == 1'b1);
+        r_true <= (design.c3.new_mark == 1'b0);
+        r_true <= (design.c3.new_index == 10);
+        r_true <= (design.c3.new_value == 21);
+        r_true <= (design.c3.new_rank == 2);
+        r_true <= (design.c3.new_low == 2);
+        r_true <= (design.c3.new_high == 2);
+        r_true <= (design.c3.handle == 3);
+        r_true <= (design.c3.array_code == 2);
+        
+        r_true <= (design.c2.new_arrDef == 1'b1);
+        r_true <= (design.c2.new_eltDef == 1'b1);
+        r_true <= (design.c2.new_mark == 1'b0);
+        r_true <= (design.c2.new_index == 4);
+        r_true <= (design.c2.new_value == 10);
+        r_true <= (design.c2.new_rank == 1);
+        r_true <= (design.c2.new_low == 1);
+        r_true <= (design.c2.new_high == 2);
+        r_true <= (design.c2.handle == 2);
+        r_true <= (design.c2.new_array_code == 1);
+        
+        r_true <= (design.c4.new_arrDef == 1'b0);
+        r_true <= (design.c4.new_eltDef == 1'b0);
+        r_true <= (design.c4.new_mark == 1'b0);
+        
+        r_true <= (design.c5.new_arrDef == 1'b0);
+        r_true <= (design.c5.new_eltDef == 1'b0);
+        r_true <= (design.c5.new_mark == 1'b0);
+
+        //ESFAArrayOp().lookUp(post_deletion_state, 0, 0) 
+        new_index <= 0;
+        isHandle <= 1'b1;
+        queried_handle <= 0;
+        selector <= 3;
+        #28;
+        selector <= 1;
+        #28;
+        selector <= 2;
+        #28;
+        
+        r_true <= resultBool;
+        r_true <= (resultValue == 5);
         
         
     end
-    
+   
     
 endmodule
