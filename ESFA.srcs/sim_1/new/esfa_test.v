447293e3abb9c45ce80889a453a3a63d3d40ed08
Justin Tjoa <justintoja@gmail.com>
2021-11-26 18:45:46 -0800

    update

--- ESFA.srcs/sim_1/new/esfa_test.v
+++ ESFA.srcs/sim_1/new/esfa_test.v
@@ -578,151 +578,171 @@ module esfa_test;
         r_true = (resultBool == 0); #168; //there is no value with index 1 in sixth array 
         
         //ESFAArrayOp().delete(state_and_handle._1, 1)
-        isMetadata = 1'b1;
+        isMetadata = 1;
         metadata = 1;
-        selector = 3;
-        willWrite = 1;
+        selector = 2;
         #168;
+        
         code = resultValue;
-        r_true = resultBool; #168;
-        r_true = 1;
-        isMetadata = 1'b1;
+        selector = 4;
+        new_index = 1;
         metadata = code;
-        selector = 5;
+        
+        willWrite = 1;
         #28;
         willWrite = 0;
-        #392   
+        #392;
+        r_true = resultBool; #168;
+        r_true = 1;
         
-        r_true = (l1.c0.new_arrDef == 1'b1);
-        r_true = (l1.c0.new_eltDef == 1'b1);
-        r_true = (l1.c0.new_mark == 1'b0);
-        r_true = (l1.c0.new_index == 0);
-        r_true = (l1.c0.new_value == 5);
-        r_true = (l1.c0.new_rank == 1);
-        r_true = (l1.c0.new_low == 0);
-        r_true = (l1.c0.new_high == 2);
-        r_true = (l1.c0.handle == 0);
-        r_true = (l1.c0.new_array_code == 0);
         
-        r_true = (l1.c1.new_arrDef == 1'b0);
-        r_true = (l1.c1.new_eltDef == 1'b1);
-        r_true = (l1.c1.new_mark == 1'b0);
-        r_true = (l1.c1.new_index == 2);
-        r_true = (l1.c1.new_value == 10);
-        r_true = (l1.c1.new_rank == 0);
-        r_true = (l1.c1.new_low == 1);
-        r_true = (l1.c1.new_high == 2);
-        r_true = (l1.c1.handle == 1);
-        r_true = (l1.c1.new_array_code == 1);
+        r_true = (l1.c0.new_arrDef == 1'b1); #168;
+        r_true = (l1.c0.new_eltDef == 1'b1); #168;
+        r_true = (l1.c0.new_mark == 1'b0); #168;
+        r_true = (l1.c0.new_index == 0); #168;
+        r_true = (l1.c0.new_value == 5); #168;
+        r_true = (l1.c0.new_rank == 1); #168;
+        r_true = (l1.c0.new_low == 0); #168;
+        r_true = (l1.c0.new_high == 2); #168;
+        r_true = (l1.c0.handle == 0); #168;
+        r_true = (l1.c0.new_array_code == 0); #168;
         
-        r_true = (l1.c3.new_arrDef == 1'b1);
-        r_true = (l1.c3.new_eltDef == 1'b1);
-        r_true = (l1.c3.new_mark == 1'b0);
-        r_true = (l1.c3.new_index == 10);
-        r_true = (l1.c3.new_value == 21);
-        r_true = (l1.c3.new_rank == 2);
-        r_true = (l1.c3.new_low == 4);
-        r_true = (l1.c3.new_high == 4);
-        r_true = (l1.c3.handle == 3);
-        r_true = (l1.c3.new_array_code == 4);
+        r_true = (l1.c1.new_arrDef == 1'b0); #168;
+        r_true = (l1.c1.new_eltDef == 1'b1); #168;
+        r_true = (l1.c1.new_mark == 1'b0); #168;
+        r_true = (l1.c1.new_index == 2); #168;
+        r_true = (l1.c1.new_value == 10); #168;
+        r_true = (l1.c1.new_rank == 0); #168;
+        r_true = (l1.c1.new_low == 1); #168;
+        r_true = (l1.c1.new_high == 2); #168;
+        r_true = (l1.c1.handle == 1); #168;
+        r_true = (l1.c1.new_array_code == 1); #168;
         
-        r_true = (l1.c2.new_arrDef == 1'b1);
-        r_true = (l1.c2.new_eltDef == 1'b1);
-        r_true = (l1.c2.new_mark == 1'b0);
-        r_true = (l1.c2.new_index == 4);
-        r_true = (l1.c2.new_value == 10);
-        r_true = (l1.c2.new_rank == 1);
-        r_true = (l1.c2.new_low == 3);
-        r_true = (l1.c2.new_high == 4);
-        r_true = (l1.c2.handle == 2);
-        r_true = (l1.c2.new_array_code == 3);
+        r_true = (l1.c3.new_arrDef == 1'b1); #168;
+        r_true = (l1.c3.new_eltDef == 1'b1); #168;
+        r_true = (l1.c3.new_mark == 1'b0); #168;
+        r_true = (l1.c3.new_index == 10); #168;
+        r_true = (l1.c3.new_value == 21); #168;
+        r_true = (l1.c3.new_rank == 2); #168;
+        r_true = (l1.c3.new_low == 4); #168;
+        r_true = (l1.c3.new_high == 4); #168;
+        r_true = (l1.c3.handle == 3); #168;
+        r_true = (l1.c3.new_array_code == 4); #168;
         
-        r_true = (l1.c4.new_arrDef == 1'b1);
-        r_true = (l1.c4.new_eltDef == 1'b1);
-        r_true = (l1.c4.new_mark == 1'b0);
-        r_true = (l1.c4.new_index == 9);
-        r_true = (l1.c4.new_value == 5);
-        r_true = (l1.c4.new_rank == 3);
-        r_true = (l1.c4.new_low == 2);
-        r_true = (l1.c4.new_high == 2);
-        r_true = (l1.c4.handle == 4);
-        r_true = (l1.c4.new_array_code == 2);
+        r_true = (l1.c2.new_arrDef == 1'b1); #168;
+        r_true = (l1.c2.new_eltDef == 1'b1); #168;
+        r_true = (l1.c2.new_mark == 1'b0); #168;
+        r_true = (l1.c2.new_index == 4); #168;
+        r_true = (l1.c2.new_value == 10); #168;
+        r_true = (l1.c2.new_rank == 1); #168;
+        r_true = (l1.c2.new_low == 3); #168;
+        r_true = (l1.c2.new_high == 4); #168;
+        r_true = (l1.c2.handle == 2); #168;
+        r_true = (l1.c2.new_array_code == 3); #168;
         
-        r_true = (l1.c5.new_arrDef == 1'b1);
-        r_true = (l1.c5.new_eltDef == 1'b1);
-        r_true = (l1.c5.new_mark == 1'b0);
-        r_true = (l1.c5.new_index == 11);
-        r_true = (l1.c5.new_value == 14);
-        r_true = (l1.c5.new_rank == 3);
-        r_true = (l1.c5.new_low == 1);
-        r_true = (l1.c5.new_high == 1);
-        r_true = (l1.c5.handle == 5);
-        r_true = (l1.c5.new_array_code == 1);
+        r_true = (l1.c4.new_arrDef == 1'b1); #168;
+        r_true = (l1.c4.new_eltDef == 1'b1); #168;
+        r_true = (l1.c4.new_mark == 1'b0); #168;
+        r_true = (l1.c4.new_index == 9); #168;
+        r_true = (l1.c4.new_value == 5); #168;
+        r_true = (l1.c4.new_rank == 3); #168;
+        r_true = (l1.c4.new_low == 2); #168;
+        r_true = (l1.c4.new_high == 2); #168;
+        r_true = (l1.c4.handle == 4); #168;
+        r_true = (l1.c4.new_array_code == 2); #168;
+        
+        r_true = (l1.c5.new_arrDef == 1'b1); #168;
+        r_true = (l1.c5.new_eltDef == 1'b1); #168;
+        r_true = (l1.c5.new_mark == 1'b0); #168;
+        r_true = (l1.c5.new_index == 11); #168;
+        r_true = (l1.c5.new_value == 14); #168;
+        r_true = (l1.c5.new_rank == 3); #168;
+        r_true = (l1.c5.new_low == 1); #168;
+        r_true = (l1.c5.new_high == 1); #168;
+        r_true = (l1.c5.handle == 5); #168;
+        r_true = (l1.c5.new_array_code == 1); #168;
+        r_true = 1;
         
         //ESFAArrayOp().delete(post_deletion_state, 4)
-        isMetadata = 1'b1;
+        isMetadata = 1;
         metadata = 4;
+        selector = 2;
+        #168;
+        
+        code = resultValue;
         selector = 4;
+        new_index = 4;
+        metadata = code;
+        
         willWrite = 1;
-        #168;
-        selector = 6;
-        #168;
+        #28;
         willWrite = 0;
+        #392;
+        r_true = resultBool; #168;
+        r_true = 1;
         
         //ESFAArrayOp().delete(post_deletion_state, 5)
-        isMetadata = 1'b1;
+        isMetadata = 1;
         metadata = 5;
+        selector = 2;
+        #168;
+        
+        code = resultValue;
         selector = 4;
+        new_index = 5;
+        metadata = code;
+        
         willWrite = 1;
-        #168;
-        selector = 6;
-        #168;
+        #28;
         willWrite = 0;
+        #392;
+        r_true = resultBool; #168;
+        r_true = 1;
         
-        r_true = (l1.c0.new_arrDef == 1'b1);
-        r_true = (l1.c0.new_eltDef == 1'b1);
-        r_true = (l1.c0.new_mark == 1'b0);
-        r_true = (l1.c0.new_index == 0);
-        r_true = (l1.c0.new_value == 5);
-        r_true = (l1.c0.new_rank == 1);
-        r_true = (l1.c0.new_low == 0);
-        r_true = (l1.c0.new_high == 0);
-        r_true = (l1.c0.handle == 0);
-        r_true = (l1.c0.new_array_code == 0);
+        r_true = (l1.c0.new_arrDef == 1'b1); #168
+        r_true = (l1.c0.new_eltDef == 1'b1); #168
+        r_true = (l1.c0.new_mark == 1'b0); #168
+        r_true = (l1.c0.new_index == 0); #168
+        r_true = (l1.c0.new_value == 5); #168
+        r_true = (l1.c0.new_rank == 1); #168
+        r_true = (l1.c0.new_low == 0); #168
+        r_true = (l1.c0.new_high == 0); #168
+        r_true = (l1.c0.handle == 0); #168
+        r_true = (l1.c0.new_array_code == 0); #168
         
-        r_true = (l1.c1.new_arrDef == 1'b0);
-        r_true = (l1.c1.new_eltDef == 1'b0);
-        r_true = (l1.c1.new_mark == 1'b0);
+        r_true = (l1.c1.new_arrDef == 1'b0); #168
+        r_true = (l1.c1.new_eltDef == 1'b0); #168
+        r_true = (l1.c1.new_mark == 1'b0); #168
         
-        r_true = (l1.c3.new_arrDef == 1'b1);
-        r_true = (l1.c3.new_eltDef == 1'b1);
-        r_true = (l1.c3.new_mark == 1'b0);
-        r_true = (l1.c3.new_index == 10);
-        r_true = (l1.c3.new_value == 21);
-        r_true = (l1.c3.new_rank == 2);
-        r_true = (l1.c3.new_low == 2);
-        r_true = (l1.c3.new_high == 2);
-        r_true = (l1.c3.handle == 3);
-        r_true = (l1.c3.new_array_code == 2);
+        r_true = (l1.c3.new_arrDef == 1'b1); #168
+        r_true = (l1.c3.new_eltDef == 1'b1); #168
+        r_true = (l1.c3.new_mark == 1'b0); #168
+        r_true = (l1.c3.new_index == 10); #168
+        r_true = (l1.c3.new_value == 21); #168
+        r_true = (l1.c3.new_rank == 2); #168
+        r_true = (l1.c3.new_low == 2); #168
+        r_true = (l1.c3.new_high == 2); #168
+        r_true = (l1.c3.handle == 3); #168
+        r_true = (l1.c3.new_array_code == 2); #168
         
-        r_true = (l1.c2.new_arrDef == 1'b1);
-        r_true = (l1.c2.new_eltDef == 1'b1);
-        r_true = (l1.c2.new_mark == 1'b0);
-        r_true = (l1.c2.new_index == 4);
-        r_true = (l1.c2.new_value == 10);
-        r_true = (l1.c2.new_rank == 1);
-        r_true = (l1.c2.new_low == 1);
-        r_true = (l1.c2.new_high == 2);
-        r_true = (l1.c2.handle == 2);
-        r_true = (l1.c2.new_array_code == 1);
+        r_true = (l1.c2.new_arrDef == 1'b1); #168
+        r_true = (l1.c2.new_eltDef == 1'b1); #168
+        r_true = (l1.c2.new_mark == 1'b0); #168
+        r_true = (l1.c2.new_index == 4); #168
+        r_true = (l1.c2.new_value == 10); #168
+        r_true = (l1.c2.new_rank == 1); #168
+        r_true = (l1.c2.new_low == 1); #168
+        r_true = (l1.c2.new_high == 2); #168
+        r_true = (l1.c2.handle == 2); #168
+        r_true = (l1.c2.new_array_code == 1); #168
         
-        r_true = (l1.c4.new_arrDef == 1'b0);
-        r_true = (l1.c4.new_eltDef == 1'b0);
-        r_true = (l1.c4.new_mark == 1'b0);
+        r_true = (l1.c4.new_arrDef == 1'b0); #168
+        r_true = (l1.c4.new_eltDef == 1'b0); #168
+        r_true = (l1.c4.new_mark == 1'b0); #168
         
-        r_true = (l1.c5.new_arrDef == 1'b0);
-        r_true = (l1.c5.new_eltDef == 1'b0);
-        r_true = (l1.c5.new_mark == 1'b0);
+        r_true = (l1.c5.new_arrDef == 1'b0); #168
+        r_true = (l1.c5.new_eltDef == 1'b0); #168
+        r_true = (l1.c5.new_mark == 1'b0); #168
 
         //ESFAArrayOp().lookUp(post_deletion_state, 0, 0) 
         new_index = 0;
