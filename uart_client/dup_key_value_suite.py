42f36905aa0550a0f49a034be7006921fc533240
Justin Tjoa <justintoja@gmail.com>
2022-01-25 16:44:15 -0800

    Update

---
+++ uart_client/dup_key_value_suite.py
@@ -0,0 +1,10 @@
+from esfa_tools import *
+
+m_update(None, 0, 5)
+m_update(0, 0, 5)
+value = m_lookUp(0, 0)
+assert(value == 5)
+value = m_lookUp(1,0)
+assert(value == 5)
+value = m_lookUp(1, 10)
+assert(value == None)
