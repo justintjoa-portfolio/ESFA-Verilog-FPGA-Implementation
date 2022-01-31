6008935bf3742eaf62f6477c1413d9267b68c9b4
Justin Tjoa <justintoja@gmail.com>
2022-01-31 00:02:57 -0800

    update

--- sim_1/new/ESFABenchmark.v
+++ sim_1/new/ESFABenchmark.v
@@ -59,25 +59,7 @@ module ESFABenchmark(
             reset = 1'b1;
             $display("assertions working correctly");
             doRun = 1'b1;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #28;
-            #48000; 
+            #560000;
             doRun = 1'b0;
             
         end
