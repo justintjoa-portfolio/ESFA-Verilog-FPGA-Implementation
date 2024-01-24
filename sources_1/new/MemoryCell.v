3f3d36d8708a4920ebff1daf8ecac5b981633dd0
justintjoa-portfolio <64376573+justintjoa-portfolio@users.noreply.github.com>
2024-01-23 22:12:59 -0800

    refactor: fixed selector logic in memory cell (#60)
    Remove the need to use clearMutation and explicitly comment
    the no op selector's role.

--- sources_1/new/MemoryCell.v
+++ sources_1/new/MemoryCell.v
@@ -38,10 +38,6 @@ module MemoryCell(
         output reg[7:0] new_result_value,
         output reg[7:0] new_context
     );
-    
-     
-     reg[0:0] r_willWrite;
-     reg[0:0] didMutate;
      
      reg[0:0] new_arrDef;
      reg[0:0] new_arrDef_next;
@@ -62,6 +58,8 @@ module MemoryCell(
      reg[0:0] new_bool_next;
      reg[7:0] new_result_value_next;
      reg[7:0] new_context_next;
+     reg[7:0] prev_selector;
+     reg[7:0] prev_selector_next;
      
     //Map
     // 0 : update  
@@ -90,26 +88,20 @@ module MemoryCell(
                     new_result_value <= 8'b0;
                     new_bool <= 1'b0;  
                     new_context <= 8'b0;
-                    didMutate <= 1'b0;
+                    prev_selector <= 8'b1000;
                 end else begin  
-                      if (r_willWrite) begin
-                           if (!didMutate) begin  
-                                didMutate <= 1'b1;
-                                new_arrDef <= new_arrDef_next;
-                                new_array_code <= new_array_code_next;
-                                new_eltDef <= new_eltDef_next;
-                                new_rank <= new_rank_next;
-                                new_low <= new_low_next;
-                                new_high <= new_high_next;
-                                new_index <= new_index_next;
-                                new_value <= new_value_next;
-                           end
-                      end else begin   
-                          didMutate <= 1'b0;
-                      end     
+                      new_arrDef <= new_arrDef_next;
+                      new_array_code <= new_array_code_next;
+                      new_eltDef <= new_eltDef_next;
+                      new_rank <= new_rank_next;
+                      new_low <= new_low_next;
+                      new_high <= new_high_next;
+                      new_index <= new_index_next;
+                      new_value <= new_value_next; 
                       new_bool <= new_bool_next;
                       new_result_value <= new_result_value_next;
                       new_context <= new_context_next;
+                      prev_selector <= prev_selector_next;
                end
     end
                 
@@ -117,7 +109,6 @@ module MemoryCell(
     
     
     always @(*) begin
-               r_willWrite = 1'b0;
                new_arrDef_next = new_arrDef;
                new_array_code_next = new_array_code;
                new_eltDef_next = new_eltDef;
@@ -129,108 +120,43 @@ module MemoryCell(
                new_bool_next = new_bool;
                new_result_value_next = new_result_value;
                new_context_next = new_context;
-               case (selector)
-                    0: begin
-                        new_bool_next = (available_handle == handle && is_available_handle);
-               
-                        if (new_bool_next) begin  
-                            new_arrDef_next = 1'b1;
-                            new_array_code_next = handle;
-                            new_eltDef_next = 1'b1;
-                            new_low_next = handle;
-                            new_high_next = handle;
-                            new_value_next = inserted_value;
-                            new_index_next = inserted_index;
-                            new_rank_next = 1;
-                        end   
-                        new_result_value_next = handle;
-                        new_context_next = handle;
-                        r_willWrite = 1'b1;
-                    end
-                    
-                    1: begin
-                        new_bool_next = (new_index_next == inserted_index) && is_given_code && (given_code >= new_low_next) && (given_code <= new_high_next);
-                        new_result_value_next = new_value_next;
-                        new_context_next = new_rank_next;
-                    
-                    end
-               
-                    2: begin
-                        new_bool_next = (!(queried_handle > 7)) && (new_arrDef_next) && (queried_handle  == handle);
-                        new_result_value_next = new_array_code_next;
-                        new_context_next = new_array_code_next;
-                    end
-                    
-                    3: begin
-                        if (is_given_code && is_given_rank) begin 
-                            if (available_handle == handle && is_available_handle) begin  
-                                new_bool_next = 1'b1; // allows so that output of this is equivalent to preceding op, update 
-                                new_array_code_next = given_code + 1;
-                                new_high_next = given_code + 1;
-                                new_low_next = given_code + 1;
-                                new_rank_next = given_rank + 1;
-                            end else begin     
-                                if (new_array_code_next > given_code && new_arrDef_next) begin 
-                                    new_array_code_next = new_array_code_next + 1;
-                                end
-                                if (new_eltDef_next) begin 
-                                    if (new_low_next > given_code) begin 
-                                        new_low_next = new_low_next + 1;
-                                    end
-                                    if (new_high_next >= given_code) begin 
-                                        new_high_next = new_high_next + 1;
-                                    end
-                                end
-                            end
-                        r_willWrite = 1'b1;
+               prev_selector_next = selector; 
+               if (selector != prev_selector) begin  
+                    case (selector)
+                        0: begin
+                            new_bool_next = (available_handle == handle && is_available_handle);
+                   
+                            if (new_bool_next) begin  
+                                new_arrDef_next = 1'b1;
+                                new_array_code_next = handle;
+                                new_eltDef_next = 1'b1;
+                                new_low_next = handle;
+                                new_high_next = handle;
+                                new_value_next = inserted_value;
+                                new_index_next = inserted_index;
+                                new_rank_next = 1;
+                            end   
+                            new_result_value_next = handle;
+                            new_context_next = handle;
                         end
-                    end
-                    
-                    4: begin
-                        if (is_given_code) begin  
-                            new_bool_next = 1'b1;
-                            if (queried_handle == handle) begin    
-                                new_arrDef_next = 1'b0;
-                                new_rank_next = 0;
-                            end
-                       
-                            if (new_eltDef_next && given_code < new_low_next) begin  
-                                new_high_next = new_high_next - 1;
-                                new_low_next = new_low_next - 1;
-                            end else begin   
-                                if (new_eltDef_next && (new_low_next <= given_code && given_code <= new_high_next)) begin 
-                                    new_high_next = new_high_next - 1;
-                                end
-                            end
-                            if (new_eltDef_next && new_low_next > new_high_next) begin 
-                                new_eltDef_next = 1'b0;
-                                new_arrDef_next = 1'b0;
-                            end
-                            if (new_arrDef_next && new_array_code_next > given_code) begin 
-                                new_array_code_next = new_array_code_next - 1;
-                            end
-
-                            r_willWrite = 1'b1;
+                        
+                        1: begin
+                            new_bool_next = (new_index_next == inserted_index) && is_given_code && (given_code >= new_low_next) && (given_code <= new_high_next);
+                            new_result_value_next = new_value_next;
+                            new_context_next = new_rank_next;
+                        
                         end
-                    end
-                
-                    5: begin
-                        new_bool_next = ! new_eltDef_next;
-                        new_result_value_next = handle;
-                        new_context_next = handle;
-                    end
-
-                    6: begin   
-                        new_bool_next = (!(queried_handle > 7)) && (new_arrDef_next) && (queried_handle == handle);
-                        new_result_value_next = new_rank_next;
-                        new_context_next = new_rank_next;
-                    end
-                    
-                    7: begin
-                        if (queried_handle == handle) begin    
+                   
+                        2: begin
+                            new_bool_next = (!(queried_handle > 7)) && (new_arrDef_next) && (queried_handle  == handle);
+                            new_result_value_next = new_array_code_next;
+                            new_context_next = new_array_code_next;
+                        end
+                        
+                        3: begin
                             if (is_given_code && is_given_rank) begin 
-                                new_bool_next = 1'b1;
-                                if (available_handle == handle && is_available_handle) begin   
+                                if (available_handle == handle && is_available_handle) begin  
+                                    new_bool_next = 1'b1; // allows so that output of this is equivalent to preceding op, update 
                                     new_array_code_next = given_code + 1;
                                     new_high_next = given_code + 1;
                                     new_low_next = given_code + 1;
@@ -250,14 +176,77 @@ module MemoryCell(
                                 end
                             end
                         end
-                    end
-                   
-                    8: begin   
-
-                    end
+                        
+                        4: begin
+                            if (is_given_code) begin  
+                                new_bool_next = 1'b1;
+                                if (queried_handle == handle) begin    
+                                    new_arrDef_next = 1'b0;
+                                    new_rank_next = 0;
+                                end
+                           
+                                if (new_eltDef_next && given_code < new_low_next) begin  
+                                    new_high_next = new_high_next - 1;
+                                    new_low_next = new_low_next - 1;
+                                end else begin   
+                                    if (new_eltDef_next && (new_low_next <= given_code && given_code <= new_high_next)) begin 
+                                        new_high_next = new_high_next - 1;
+                                    end
+                                end
+                                if (new_eltDef_next && new_low_next > new_high_next) begin 
+                                    new_eltDef_next = 1'b0;
+                                    new_arrDef_next = 1'b0;
+                                end
+                                if (new_arrDef_next && new_array_code_next > given_code) begin 
+                                    new_array_code_next = new_array_code_next - 1;
+                                end
+    
+                            end
+                        end
                     
-                    
+                        5: begin
+                            new_bool_next = ! new_eltDef_next;
+                            new_result_value_next = handle;
+                            new_context_next = handle;
+                        end
+    
+                        6: begin   
+                            new_bool_next = (!(queried_handle > 7)) && (new_arrDef_next) && (queried_handle == handle);
+                            new_result_value_next = new_rank_next;
+                            new_context_next = new_rank_next;
+                        end
+                        
+                        7: begin
+                            if (queried_handle == handle) begin    
+                                if (is_given_code && is_given_rank) begin 
+                                    new_bool_next = 1'b1;
+                                    if (available_handle == handle && is_available_handle) begin   
+                                        new_array_code_next = given_code + 1;
+                                        new_high_next = given_code + 1;
+                                        new_low_next = given_code + 1;
+                                        new_rank_next = given_rank + 1;
+                                    end else begin     
+                                        if (new_array_code_next > given_code && new_arrDef_next) begin 
+                                            new_array_code_next = new_array_code_next + 1;
+                                        end
+                                        if (new_eltDef_next) begin 
+                                            if (new_low_next > given_code) begin 
+                                                new_low_next = new_low_next + 1;
+                                            end
+                                            if (new_high_next >= given_code) begin 
+                                                new_high_next = new_high_next + 1;
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end           
+                        
+                        8: begin 
+                            // no op selector 
+                        end         
                 endcase
+               end 
            end
 
             
