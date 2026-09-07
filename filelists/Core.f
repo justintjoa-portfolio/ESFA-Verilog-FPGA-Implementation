31a650692e30a15c232fdd87fb271b83f6d49c02
Justin Tjoa <justintoja@gmail.com>
2026-09-07 13:27:02 -0700

    s

--- filelists/Core.f
+++ filelists/Core.f
@@ -1,7 +1,6 @@
 src/wiring_interfaces/CombinatorIO.sv
 src/wiring_interfaces/MemoryCellInput.sv
 src/wiring_interfaces/EsfaDesignIO.sv
-src/platform/xilinx/ip/blk_mem_gen_0/blk_mem_gen_0.xcix
 
 src/core/NodeCombinator.sv
 src/core/MemoryCell.sv
